218c58c8ce1b5831a302aa8cd7fc415d7be37a17
Sashank <sashank@Sashanks-MacBook-Pro.local>
2023-03-29 09:58:31 +0530

    added sql_model file and removed reff to main_id

--- views/stg_order_completed_rows.sql
+++
@@ -1,9 +0,0 @@
--- WHT currently doesn't support some sql operations such as exploding rows of an array/json to create more rows.
--- A temporary fix to support those is by creating views and using them for the features
-
-create view stg_order_completed_rows as 
-   select 
-   timestamp, 
-   user_id,
-    t.value['category_l1'] as category_l1, t.value['product_id'] as product_id, t.value['price'] as amount_spent
-    from order_completed, TABLE(FLATTEN(parse_json(properties_products))) t
